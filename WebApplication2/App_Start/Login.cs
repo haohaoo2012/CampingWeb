694b060b8ba93674cb28767f5c6ce2420240511c
haohaoo2012 <73508026+haohaoo2012@users.noreply.github.com>
2021-04-27 22:20:24 +0700

    Add project files.

---
+++ WebApplication2/App_Start/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.App_Start
+{
+    public class Login
+    {
+        public string user { get; set; }
+        public string pass { get; set; }
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using WebApplication2.App_Start;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        WebEntities db = new WebEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+       
+        public ActionResult Sign()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Sign(login log)
+        {
+            var login = db.logins.Where(x => x.user == log.user && x.pass == log.pass).Count();
+            if (login > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
